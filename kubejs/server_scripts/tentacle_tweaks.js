b19a6d9655ac83057dd8af086a29fd0ab17a89df
Octelly <eli@stefek.cz>
2024-03-11 17:54:53 +0100

    1.0-rc10

---
+++ kubejs/server_scripts/tentacle_tweaks.js
@@ -0,0 +1,12 @@
+ServerEvents.recipes(event => {
+    function compacting(smol, big) {
+        return event.custom({
+            type: 'functionalstorage:custom_compacting',
+            higher_input: Item.of(big).toJson(),
+            lower_input: Item.of(smol).toJson()
+        })
+    }
+
+    compacting('8x spelunkery:coal_lump', 'minecraft:coal')
+    compacting('8x kubejs:charcoal_lump', 'minecraft:charcoal')
+})
