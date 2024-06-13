cfa1539a0a017c1d6ef8f58545ef421449f0af02
Octelly (Eli Štefků) <eli@stefek.cz>
2024-06-13 17:46:50 +0200

    small fixes - ammended advanced monitor recipe - fixed char/coal lumps compacting in storage drawers - removed old traffic mod - integrated vintage more into progression (related upstream feature request: https://github.com/Creators-of-Create/Create/issues/6593 )

--- kubejs/server_scripts/tentacle_vintage.js
+++ kubejs/server_scripts/tentacle_vintage.js
@@ -1,4 +1,25 @@
 ServerEvents.recipes(event => {
+
+	const hammering = (results, blows, ingredients) => {
+		if (Array.isArray(results) != true) {
+			results = [results]
+		}
+		if (Array.isArray(ingredients) != true) {
+			ingredients = [ingredients]
+		}
+
+		return event.custom({
+			type: 'vintageimprovements:hammering',
+			hammerBlows: blows,
+			ingredients: ingredients.map(x => {
+				return Ingredient.of(x).toJson()
+			}),
+			results: results.map(x => {
+				return Ingredient.of(x).toJson()
+			})
+		})
+	}
+
 	event.remove({ type: 'vintageimprovements:pressurizing' })
 
 	event.recipes.create.mixing('8x minecraft:gunpowder', [
@@ -29,4 +50,24 @@ ServerEvents.recipes(event => {
 			}
 		]
 	}).id('vintageimprovements:centrifugation/ender_eye')
+
+	event.remove({ output: '#forge:plates/steel' })
+	hammering('alloyed:steel_sheet', 2, 'alloyed:steel_ingot')
+
+	event.remove({ output: '#forge:plates/cast_iron' })
+	hammering('createdeco:cast_iron_sheet', 1, 'createdeco:cast_iron_ingot')
+
+	event.remove({ not: { output: 'create:sandpaper' }, output: '#create:sandpaper' })
+	event.replaceInput({},
+		'#create:sandpaper',
+		'create:sandpaper'
+	)
+	event.replaceInput({ id: 'create:crafting/materials/sand_paper' },
+		'#forge:sand/colorless',
+		'#forge:sand'
+	)
+
+	// doesn't work
+	//event.remove({ id: /.*sandpaper_polishing.*using_deployer/ })
+	event.remove({ type: 'create:deploying', id: /.*sandpaper_polishing.*using_deployer/ })
 })
