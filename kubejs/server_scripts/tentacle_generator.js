9d50a1a1987c7319c699f53ce4bda96847b70e28
Octelly <octelly@owo.digital>
2025-08-06 20:07:50 +0200

    generator fix

--- kubejs/server_scripts/tentacle_generator.js
+++ kubejs/server_scripts/tentacle_generator.js
@@ -1,10 +1,12 @@
 ServerEvents.recipes(event => {
+    // doesn't work if it's a full bucket
+    // seems to be a Create oversight
     event.recipes.create.mixing([
         'minecraft:cobblestone',
-        Fluid.of('minecraft:water', 1000),
-        Fluid.of('minecraft:lava', 1000)
+        Fluid.of('minecraft:water', 500),
+        Fluid.of('minecraft:lava', 500)
     ], [
-        Fluid.of('minecraft:water', 1000),
-        Fluid.of('minecraft:lava', 1000)
+        Fluid.of('minecraft:water', 500),
+        Fluid.of('minecraft:lava', 500)
     ]).id('tentacle_flavoured:generators/cobblestone')
 })
